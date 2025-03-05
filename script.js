8bd41b7509e25218e6d2f7cabed21da9845e84b9
Max <kotiik97@mail.ru>
2025-03-05 22:16:08 +0300

    меняться фон в зависимость от времени года

--- script.js
+++ script.js
@@ -1,222 +1,10 @@
-// (function (selector) {
-//     initCalendar(document.querySelector(selector));
-    
-//     function initCalendar(calendar) {
-//         let yearTitle = calendar.querySelector('.calendar-triple_header-title h1');
-//         let prevBtn = calendar.querySelector('.calendar-triple_month-arrows__prev');
-//         let nextBtn = calendar.querySelector('.calendar-triple_month-arrows__next');
-    
-//         let date = new Date();
-//         let showedYear = date.getFullYear();
-//         let showedDate = date.getDate();
-        
-//         let showedMonthPrev = date.getMonth() - 1;
-//         let showedMonth = date.getMonth();
-//         let showedMonthNext = date.getMonth() + 1;
-        
-//         let arrMonths = [showedMonthPrev, showedMonth, showedMonthNext];
-
-//         drawCalendar(showedYear, arrMonths, calendar, yearTitle, showedDate, showedMonth);
-        
-//         prevBtn.addEventListener('click', () => {
-//             showedYear = getPrevYear(showedYear, arrMonths[0]);
-//             for (let i = 0; i < arrMonths.length; i++) {
-//                 arrMonths[i] = getPrevMonth(arrMonths[i]);
-//             }
-    
-//             drawCalendar(showedYear, arrMonths, calendar, yearTitle, showedDate);
-//         })
-        
-//         nextBtn.addEventListener('click', () => {
-//             showedYear = getNextYear(showedYear, arrMonths[2]);
-//             for (let i = 0; i < arrMonths.length; i++) {
-//                 arrMonths[i] = getNextMonth(arrMonths[i]);
-//             }
-    
-//             drawCalendar(showedYear, arrMonths, calendar, yearTitle, showedDate);
-//         })
-//     }
-
-
-//     function getPrevYear(year, month) {
-//         if (month == 0) {
-//             return year - 1;
-//         } else {
-//             return year;
-//         }
-//     }
-
-//     function getPrevMonth(month) {
-//         if (month == 0) {
-//             return 11;
-//         } else {
-//             return month - 1;
-//         }
-//     }
-
-//     function getNextYear(year, month) {
-//         if (month == 11) {
-//             return year + 1;
-//         } else {
-//             return year;
-//         }
-//     }
-
-//     function getNextMonth(month) {
-//         console.log(month)
-//         if (month == 11) {
-//             return 0;
-//         } else {
-//             return month + 1;
-//         }
-//     }
-
-//     function drawCalendar(year, months, calendar, yearTitle, day, currentMonth) {
-//         let dates = calendar.querySelectorAll('.calendar-triple_month');
-//         let monthsTitle = document.querySelectorAll('.calendar-triple_month-title');
-//         console.log(months)
-//         months.forEach((month, index) => {
-//             drawDates(year, month, dates[index], index, day, currentMonth);
-
-//             showInfo(year, month, monthsTitle[index], yearTitle);
-//         })
-//     }
-
-//     function showInfo(year, month, elemMonth, elemYear) {
-//         elemMonth.textContent = getNameMonths(month);
-//         elemYear.textContent = year;
-//     }
-
-    // function getNameMonths(num) {
-    //     let nameMonths = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-    //     return nameMonths[num];
-    // }
-
-//     function drawDates(year, month, dates, index, day, currentMonth) {
-//         let arr = [];
-//         let firstDateOfMonth = 1;
-//         let lastDateOfMonth = getLastDayOfMonth(year, month);
-//         let unshiftElemsNum = getUnshiftElemsNum(year, month);
-//         let pushElemsNum = getPushElemsNum(year, month);
-//         arr = createArr(firstDateOfMonth, lastDateOfMonth);
-//         arr = unshiftElems(unshiftElemsNum, '', arr);
-//         arr = pushElems(pushElemsNum, '', arr);
-//         arr = pieceArr(7, arr);
-
-//         createDay(arr, dates, index, day, currentMonth);
-//     }
-
-//     function createDay(arr, parent, index, day, currentMonth) {
-        // let weekDivShadow = document.createElement('div');
-        // weekDivShadow.classList.add('calendar-triple_month__shadow-week');
-//         parent.innerHTML = '';
-//         for (let [keyMonth, subArr] of arr.entries()) {
-//             let weekDiv = document.createElement('div');
-//             weekDiv.classList.add('calendar-triple_month-week');
-//             for (let [keyDay, elem] of subArr.entries()) {
-//                 let div = document.createElement('div');
-
-//                 if (keyDay == 5 || keyDay == 6) {
-//                     div.classList.add('calendar-triple_month-weekend');
-                    
-//                 };
-                // if (currentMonth) {
-                //     if (elem == day && index == 1) {
-                //         weekDiv.append(weekDivShadow)
-                //         div.classList.add('currentDate');
-                        
-                //     }
-                // }
-//                 div.classList.add('calendar-triple_month-day');
-//                 div.textContent = elem;
-//                 weekDiv.append(div);
-//             }
-//             parent.append(weekDiv);
-//         }
-//     }
-
-//     function createArr(from, to) {
-//         let arr = [];
-//         for (let i = from; i <= to; i++) {
-//             arr.push(i);
-//         }
-
-//         return arr;
-//     }
-
-//     function unshiftElems(num, elem, arr) {
-//         for (let i = 0; i < num; i++) {
-//             arr.unshift(elem);
-//         }
-
-//         return arr;
-//     }
-
-//     function pushElems(num, elem, arr) {
-//         for (let i = 0; i < num; i++) {
-//             arr.push(elem);
-//         }
-
-//         return arr;
-//     }
-
-//     function getLastDayOfMonth(year, month) {
-//         let date = new Date(year, month + 1, 0);
-//         return date.getDate();
-//     }
-
-//     function getUnshiftElemsNum(year, month) {
-//         let jsDayNum = getFirstWeekDayOfMonthNum(year, month);
-//         let realDayNum = getRealDayOfWeekNum(jsDayNum);
-
-//         return realDayNum - 1;
-//     }
-
-//     function getPushElemsNum(year, month) {
-//         let jsDayNum = getLastWeekDayOfMonthNum(year, month);
-//         let realDayNum = getRealDayOfWeekNum(jsDayNum);
-
-//         return 7 - realDayNum;
-//     }
-
-//     function pieceArr(num, arr) {
-//         let result = [];
-//         let piece = [];
-//         let iterCount = arr.length / num;
-
-//         for (let i = 0; i < iterCount; i++) {
-//             piece = arr.splice(0, num);
-//             result.push(piece);
-//         }
-
-//         return result;
-//     }
-
-//     function getRealDayOfWeekNum(jsNumOfDay) {
-//         if (jsNumOfDay == 0) {
-//             return 7;
-//         } else {
-//             return jsNumOfDay;
-//         }
-//     }
-
-//     function getFirstWeekDayOfMonthNum(year, month) {
-//         let date = new Date(year, month, 1);
-//         return date.getDay();
-//     }
-
-//     function getLastWeekDayOfMonthNum(year, month) {
-//         let date = new Date(year, month + 1, 0);
-//         return date.getDay();
-//     }
-
-// }('.calendar-triple'));
-
+"use strict"
 
 let calendar = document.querySelector('.calendar-triple');
 let dates = calendar.querySelectorAll('.calendar-triple_month');
 let monthTitle = calendar.querySelectorAll('.calendar-triple_month-title');
 let yearTitle = calendar.querySelector('.calendar-triple_header-title h1');
+let headerBg = document.querySelector('.calendar-triple_header');
 
 let prevBtn = document.querySelector('.calendar-triple_month-arrows__prev');
 let nextBtn = document.querySelector('.calendar-triple_month-arrows__next');
@@ -249,13 +37,27 @@ let arrFirstDayOfMonth = [prevStartMonth, currentStartMonth, nextStartMonth];
 let arrLastDayOfMonth = [prevEndDayOfMonth, currentEndDayOfMonth, nextEndDayOfMonth];
 
 let day = date.getDay();
+function backgroundTimeYear(months) {
+    for (let i = 0; i < months.length; i++) {
+        if (months[1] == 0 || months[1] == 1 || months[1] == 11) {
+            headerBg.dataset.type ="bgZima";
+        } else if (months[1] == 2 || months[1] == 3 || months[1] == 4) {
+            headerBg.dataset.type ="bgVesna";
+        } else if (months[1] == 5 || months[1] == 6 || months[1] == 7 ) {
+            headerBg.dataset.type ="bgLeto";
+        } else if (months[1] == 8 || months[1] == 9 || months[1] == 10 ) {
+            headerBg.dataset.type ="bgOsen";
+        }
+    }
+}
+
+backgroundTimeYear(arrMonths);
 
 nextBtn.addEventListener('click', () => {
-    year = nextYear(year, arrMonths[2]);
-    // if (arrMonths[2] == 11 && arrMonths[1] == 10) {
-    //     year = nextYear(year, arrMonths[2]);
-    // }
     for (let i = 0; i < arrMonths.length; i++) {
+        if (arrMonths[i] == 11) {
+            year = nextYear(year, arrMonths[2]);
+        }
         arrMonths[i] = nextMonths(arrMonths[i]);
         prevMonthCountDays = new Date(year, month + arrMonths[0], 0).getDate();
         currentMonthCountDays = new Date(year, month + arrMonths[1], 0).getDate();
@@ -264,8 +66,8 @@ nextBtn.addEventListener('click', () => {
     }
 
     showInfo(year, arrMonths, monthTitle, yearTitle);
-    drawCalendar(arrCountDays, month)
-
+    drawCalendar(arrCountDays, nextMonth);
+    backgroundTimeYear(arrMonths);
 });
 
 function nextYear(year, month) {
@@ -285,11 +87,10 @@ function nextMonths (month) {
 }
 
 prevBtn.addEventListener('click', () => {
-    year = prevYear(year, arrMonths[0]);
-    // if (arrMonths[2] == 0 && arrMonths[1] == 11) {
-    //     year = prevYear(year, arrMonths[2]);
-    // }
     for (let i = 0; i < arrMonths.length; i++) {
+        if (arrMonths[i] == 0) {
+            year = prevYear(year, arrMonths[0]);
+        }
         arrMonths[i] = prevMonths(arrMonths[i]);
         prevMonthCountDays = new Date(year, month + arrMonths[0], 0).getDate();
         currentMonthCountDays = new Date(year, month + arrMonths[1], 0).getDate();
@@ -298,8 +99,9 @@ prevBtn.addEventListener('click', () => {
     }
 
     showInfo(year, arrMonths, monthTitle, yearTitle);
-    console.log(month)
-    drawCalendar(arrCountDays, month)
+    drawCalendar(arrCountDays, prevMonth);
+    backgroundTimeYear(arrMonths);
+
 
 });
 
@@ -380,10 +182,9 @@ function getMonths(parent, arr, index, currentMonth) {
                 }
             }
             if (currentMonth) {
-                console.log(currentMonth)
-                if (elem == currentDate && index == 1 ) {
-                    week.append(weekShadow);
+                if (elem == currentDate && index == 1) {
                     weekDay.classList.add('currentDate');
+                    week.append(weekShadow);
                 }
             }
             week.append(weekDay);
